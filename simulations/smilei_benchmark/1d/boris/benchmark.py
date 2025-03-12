4cbbeba98a6087090e5d48336aa1b3c87875169a
Frida Brogren <x_fribr@tetralith3.nsc.liu.se>
2025-03-12 18:39:03 +0100

    _

--- simulations/smilei_benchmark/1d/boris/benchmark.py
+++ simulations/smilei_benchmark/1d/boris/benchmark.py
@@ -7,7 +7,6 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 import numpy as np
 from numba import cfunc, carray, types as nbt
-from pipic.extensions import moving_window
 import h5py
 
 
@@ -24,68 +23,76 @@ def create_hdf5(fp, shape, dsets=['Ex','Ez','Ey','Bx','Bz','By','rho'],mode="w")
     file.close()
 
 
-# Smilei parameters
-dz_s = 0.6/8            # longitudinal resolution
-dy_s = 4./2
-dx_s = dy_s              # transverse resolution
-dt = 0.2*dz_s          # timestep
-nz,ny,nx = 2*32*16,2*32,2*32 #32,32
-Lx = nx*dx_s 
-Ly = ny*dy_s
-Lz = nz*dz_s
-a0 = 4.0
-n0_s = 0.003 #10e18 # [1/cm^3]
-n1_s = n0_s/3
-n0_r = 1e18/n0_s # 10e18 is the max electron (num) density
-omega_s = 2.5
+# constants
+c = 299792458*1e2 #cm/s
+m = 9.1093837*1e-31*1e3 #g
+e = 4.80320425*1e-10  # statCoulomb
 
-c = consts.light_velocity # [cm/s]
-m = consts.electron_mass # [g]
-e = consts.electron_charge # [statC]
-omega_r = np.sqrt(np.pi*4*e**2*n0_r/m)
-fwhm_duration_laser_pulse_s = 13
-waist_s = 25
+### Smilei parameters
+# references
+wl = 0.8e-4 # cm
+omega_r = 2*np.pi*c/wl # rad/s
+
+# conversions
+Nr = m*omega_r**2/(np.pi*4*e**2) # 1/cm³
+Kr = 0.51099895000 # m*c**2, MeV 
+Lr = c/omega_r # cm/2*pi
+Tr = 1/omega_r # s/2*pi
+Er = m*c*omega_r/e #statV/cm = (sqrt(g/cm)/s)
+
+# smilei simulation parameters
+dx_s = 2*np.pi/64           # longitudinal resolution
+dt_s = dx_s/4         # timestep
+nx = 4*4*32*32
+Lx = nx*dx_s
+
+n0 = 4e18
+n1 = n0/3
+
+n0_s = n0/Nr
+a0 = 4.
+omega_s = 1. 
+fwhm_duration_laser_pulse_s = 15*2*np.pi
+waist_s = 20*2*np.pi #4*fwhm_duration_laser_pulse/2.335
 distance_laser_peak_window_border_s = 1.7*fwhm_duration_laser_pulse_s
-
-# conversion factors
-Lr = c/omega_r
-Tr = 1/omega_r
-Nr = m*omega_r**2/(np.pi*4*e**2)
 
 #===========================SIMULATION INITIALIZATION===========================
 xmin, xmax = -Lx*Lr/2, Lx*Lr/2
-ymin, ymax = -Ly*Lr/2, Ly*Lr/2
-zmin, zmax = -Lz*Lr/2, Lz*Lr/2
-dx, dy, dz = (xmax - xmin)/nx, (ymax - ymin)/ny, (zmax - zmin)/nz
-timestep = dt*Tr
-thickness = 10 # thickness (in dx) of the area where the density and field is restored/removed 
+dx = (xmax - xmin)/nx
+timestep = dt_s*Tr
 
-s = 30000 #3000*10 # number of iterations 
-checkpoint = 50   
+s = 4*20000 #3000*10 # number of iterations 
+checkpoint = 100  
+
 
 
 #---------------------setting solver and simulation region----------------------
-sim=pipic.init(solver='ec',nx=nx,ny=ny,nz=nz,xmin=xmin,xmax=xmax,ymin=ymin,ymax=ymax,zmin=zmin,zmax=zmax)
+sim=pipic.init(solver='fourier_boris',nx=nx,xmin=xmin,xmax=xmax)
 
 #--------------------------- setting plasma profile --------------------------
 
-n0 = Nr*n0_s
-n1 = Nr*n1_s
-#debye_length = .1*wavelength/64.0 #>>3/(4*pi*density), <dx ???
-temperature = 0 #4 * np.pi * density * (consts.electron_charge ** 2) * debye_length ** 2
-particles_per_cell = 8 # 8 in smilei
 
-begin_upramp = zmax
-end_upramp = Lz*Lr*4 + begin_upramp
+
+
+begin_upramp = xmin/2
+end_upramp = Lx*Lr*4 + begin_upramp
 begin_downramp = end_upramp
 end_downramp = begin_downramp
-endplateau = end_downramp + 4*Lz*Lr
+endplateau = end_downramp + 4*Lx*Lr
 finish = endplateau
+
+
+debye_length = .1*wl/64.0 #>>3/(4*pi*density), <dx ???
+temperature = 0 #4 * np.pi * n0 * (consts.electron_charge ** 2) * debye_length ** 2
+totalNumberOfParticles = n0*(xmax-begin_upramp)**2/(end_upramp-begin_upramp)/2
+particles_per_macro = 0.5e10
+particles = totalNumberOfParticles/particles_per_macro
+
 
 @cfunc(types.add_particles_callback)
 def density_profile(r, data_double, data_int):
     # r is the position in the 'lab frame'  
-    R = r[2] #+ thickness*dz
+    R = r[0] #+ thickness*dz
 
     if R >= begin_upramp and R < end_upramp:
         return n0*((R-begin_upramp)/(end_upramp-begin_upramp))
@@ -97,10 +104,11 @@ def density_profile(r, data_double, data_int):
         return n1
     else:
         return 0
+    
 #---------------------------setting field of the pulse--------------------------
 
 # conversions laser parameters
-omega_l = omega_s * omega_r # [1/s]
+omega_l = omega_s * omega_r # [rad/s]
 fieldAmplitude = -a0*m*c*omega_l/e # [statV/cm]
 wavelength = 2*np.pi*consts.light_velocity/omega_l # [cm]
 
@@ -108,112 +116,76 @@ fwhm_duration_laser_pulse = Tr*fwhm_duration_laser_pulse_s
 pulseWidth_x = (fwhm_duration_laser_pulse/2.355)*consts.light_velocity # [cm]
 
 waist = waist_s*Lr
-init_laser_pos = zmax-distance_laser_peak_window_border_s*Lr
+init_laser_pos = xmin+distance_laser_peak_window_border_s*Lr - c*8*dt_s*Tr 
 focusPosition = begin_downramp - init_laser_pos
 
-omega_p = np.sqrt(4*np.pi*Nr*n0_s*consts.electron_charge**2/consts.electron_mass)
-wp = 2*np.pi*consts.light_velocity/omega_p
-print(wavelength,dz,2*np.pi/omega_l,timestep)
 
 @cfunc(types.field_loop_callback)
 def initiate_field_callback(ind, r, E, B, data_double, data_int):
 
     if data_int[0] == 0:       
-        x = r[2] - init_laser_pos
-        rho2 = r[1]**2 + r[0]**2
+        x = r[0] - init_laser_pos
         
         k = 2*np.pi/wavelength
         # Rayleigh length
-        Zr = np.pi*waist**2/wavelength 
-        
-        # curvature
-        R = focusPosition*(1+(Zr/focusPosition)**2)        
-        
-        spotsize_init = waist*np.sqrt(1+(focusPosition/Zr)**2)
+             
+      
+        amp = fieldAmplitude
 
-        phase = np.arctan(focusPosition/Zr)    
-        amp = fieldAmplitude*(waist/spotsize_init)*np.exp(-rho2/spotsize_init**2)
-        curvature = np.exp(1j*k*rho2/(2*R))
+        gp = np.real(amp*np.exp(-1j*(k*x))*np.exp(-x**2/(2*pulseWidth_x**2)))
 
-        gp = np.real(amp*curvature*np.exp(-1j*(k*x + phase))*np.exp(-x**2/(2*pulseWidth_x**2)))
-
-        # x-polarized 
-        E[0] = gp       
-        B[1] = gp        
-
-
-@cfunc(types.field_loop_callback)
-def remove_field(ind, r, E, B, data_double, data_int):
-
-    rollback = np.floor(data_int[0]*timestep*consts.light_velocity/dz)
-    if rollback%(thickness//2)==0:
-        r_rel = zmin + dz*(rollback%nz)  
-        r_min = r_rel - thickness*dz
-        r_max = r_rel 
-        if (r[2] > r_min and r[2] < r_max) or (r[2] > zmax - (zmin - r_min)) or (r[2] < zmin + (r_max - zmax)): 
-            E[1] = 0
-            B[2] = 0
-            E[2] = 0 
-            B[1] = 0
-            E[0] = 0
-            B[0] = 0 
+        # y-polarized 
+        E[1] = -gp       
+        B[2] = -gp        
 
 
 #=================================OUTPUT========================================
 #-------------------------preparing output of fields (x)-----------------------------
-Ex = np.zeros((ny,nz), dtype=np.double) 
-Ey = np.zeros((ny,nz), dtype=np.double) 
-Ez = np.zeros((ny,nz), dtype=np.double) 
+Ex = np.zeros((nx,), dtype=np.double) 
+Ey = np.zeros((nx,), dtype=np.double) 
+Ez = np.zeros((nx,), dtype=np.double) 
 
-rho = np.zeros((ny,nz), dtype=np.double) 
+rho = np.zeros((nx,), dtype=np.double) 
 # not done
 pmin = 0
 pmax = 1e-15
 nps = 2**8
-ps = np.zeros((nps,nz), dtype=np.double) 
-
+ps = np.zeros((nps,nx), dtype=np.double) 
+weights = np.zeros((nx,), dtype=np.double) 
 #------------------get functions-----------------------------------------------
     
 @cfunc(types.particle_loop_callback)
 def get_density(r, p, w, id, data_double, data_int):   
     ix = int(nx*(r[0] - xmin)/(xmax - xmin))
-    iy = int(ny*(r[1] - ymin)/(ymax - ymin))
-    iz = int(nz*(r[2] - zmin)/(zmax - zmin))
-
     data = carray(data_double, rho.shape, dtype=np.double)
     
-    if (iy < rho.shape[0] and 
-        ix == nx//2 and
-        iz < rho.shape[1]):
-        data[iy, iz] += w[0]/(dx*dy*dz)
+    if ix < rho.shape[0]:
+        data[ix] += w[0]/(dx)
 
 @cfunc(types.particle_loop_callback)
 def get_phase_space(r, p, w, id, data_double, data_int):   
-    iz = int(ps.shape[0]*(r[2] - zmin)/(zmax - zmin))
-    ip = int(ps.shape[1]*(p[2] - pmin)/(pmax - pmin))
+    ix = int(ps.shape[1]*(r[0] - xmin)/(xmax - xmin))
+    ip = int(ps.shape[0]*(p[0] - pmin)/(pmax - pmin))
     data = carray(data_double, ps.shape, dtype=np.double)
-    
-    if ip>=0 and ip < ps.shape[1] and iz < ps.shape[0]:
-        data[ip,iz] += w[0]/(dx*dy*dz) 
+
+    if ip>=0 and ip < ps.shape[0] and ix < ps.shape[1]:
+        data[ip,ix] += w[0]/(dx) 
 
 
 @cfunc(types.field_loop_callback)
 def get_field_Ey(ind, r, E, B, data_double, data_int):
-    if ind[0]==nx//2:
-        _E = carray(data_double, Ey.shape, dtype=np.double)
-        _E[ind[1], ind[2]] = E[1]
+    _E = carray(data_double, Ey.shape, dtype=np.double)
+    _E[ind[0]] = E[1]
 
 @cfunc(types.field_loop_callback)
 def get_field_Ex(ind, r, E, B, data_double, data_int):   
-    if ind[0]==nx//2:    
-        _E = carray(data_double, Ex.shape, dtype=np.double)
-        _E[ind[1], ind[2]] = E[0]
+    _E = carray(data_double, Ex.shape, dtype=np.double)
+    _E[ind[0]] = E[0]
 
 @cfunc(types.field_loop_callback)
 def get_field_Ez(ind, r, E, B, data_double, data_int):
-    if ind[0]==nx//2:      
-        _E = carray(data_double, Ez.shape, dtype=np.double)
-        _E[ind[1], ind[2]] = E[2]
+    _E = carray(data_double, Ez.shape, dtype=np.double)
+    _E[ind[0]] = E[2]
 
 
 def load_fields():
@@ -224,12 +196,7 @@ def load_fields():
 #===============================SIMULATION======================================
 
 data_int = np.zeros((1, ), dtype=np.intc) # data for passing the iteration number
-window_speed = consts.light_velocity #speed of moving window
-
 #-----------------------adding the handler of extension-------------------------
-
-# variable for passing density to the handler  
-data_double = np.zeros((1, ), dtype=np.double)
 
 #-----------------------initiate field and plasma-------------------------
 
@@ -243,20 +210,11 @@ sim.fourier_solver_settings(divergence_cleaning=0, sin2_kfilter=-1)
 # This part is just for initiating the electron species, 
 # so that the algorithm knows that there is a species called electron
 # it is therefore not important where the electrons are or what density they have
-sim.add_particles(name='electron', number=int(ny*nz*nx),
+sim.add_particles(name='electron', number=int(particles),
                 charge=consts.electron_charge, mass=consts.electron_mass,
                 temperature=temperature, density=density_profile.address,
                 data_int=pipic.addressof(data_int))
 
-density_handler_adress = moving_window.handler(sim.ensemble_data(),
-                                               thickness=thickness,
-                                               particles_per_cell=particles_per_cell,
-                                               temperature=temperature,
-                                               density = density_profile.address,)
-sim.add_handler(name=moving_window.name, 
-                subject='electron,cells',
-                handler=density_handler_adress,
-                data_int=pipic.addressof(data_int),)
 
 sim.field_loop(handler=initiate_field_callback.address, data_int=pipic.addressof(data_int),
                 use_omp=True)
@@ -268,28 +226,19 @@ dsets = ['Ex','Ey','Ez','rho','ps']
 fields = [Ex,Ey,Ez,rho,ps]
 ncp = s//checkpoint
 
-hdf5_fp = 'lwfa.h5'
-create_hdf5(hdf5_fp, shape=(ncp,ny,nz),dsets=dsets[:-1])
-create_hdf5(hdf5_fp, shape=(ncp,nps,nz),dsets=['ps'],mode="r+")
+hdf5_fp = 'lwfa_neg_delayed.h5'
+create_hdf5(hdf5_fp, shape=(ncp,nx),dsets=dsets[:-1])
+create_hdf5(hdf5_fp, shape=(ncp,nps,nx),dsets=['ps'],mode="r+")
 
 create_hdf5(hdf5_fp,shape=(nx,),dsets=['x_axis',],mode="r+")
-create_hdf5(hdf5_fp,shape=(ny,),dsets=['y_axis',],mode="r+")
-create_hdf5(hdf5_fp,shape=(nz,),dsets=['z_axis',],mode="r+")
 create_hdf5(hdf5_fp,shape=(nps,),dsets=['p_axis',],mode="r+")
 
 x_axis = np.linspace(xmin, xmax, nx)
-y_axis = np.linspace(ymin, ymax, ny)
-z_axis = np.linspace(zmin, zmax, nz)
 p_axis = np.linspace(pmin, pmax, nps)
 
 with h5py.File(hdf5_fp,"r+") as file:
     file['x_axis'][:] = x_axis
-    file['y_axis'][:] = y_axis
-    file['z_axis'][:] = z_axis
     file['p_axis'][:] = p_axis
-
-
-
 
 
 for i in range(s):
@@ -298,10 +247,7 @@ for i in range(s):
 
     sim.advance(time_step=timestep, number_of_iterations=1,use_omp=True)
     
-    sim.field_loop(handler=remove_field.address, data_int=pipic.addressof(data_int),
-                use_omp=True)
     
-  
     if i%checkpoint==0:
         print(i)
         rho.fill(0)
@@ -312,8 +258,7 @@ for i in range(s):
         sim.particle_loop(name='electron', handler=get_phase_space.address,
                     data_double=pipic.addressof(ps))
         load_fields()
-        roll_back =  int(np.floor(i*timestep*window_speed/dz))  
-
+        roll_back =  0 
         try:
             with h5py.File(hdf5_fp,"r+") as file:
                 for j,f in enumerate(dsets):
---
+++ simulations/smilei_benchmark/1d/plot_benchmark.py
@@ -0,0 +1,194 @@
+import h5py
+import numpy as np
+import matplotlib.pyplot as plt
+from pipic import consts,types
+import happi
+import matplotlib
+
+# add folder to path
+import sys
+sys.path.append('../../../../plot_style/')
+import style
+
+style.load_preset(scale=1)
+
+
+def load_data(fp,fields,axes=[],ind=-1):
+    with h5py.File(fp,"r") as file: 
+        fields_out = []
+        axes_out = []
+        for field in fields:
+            fields_out.append(file[field][:ind])
+        for a in axes:
+            axes_out.append(file[a][:])
+    return fields_out,axes_out
+
+
+# constants
+c = 299792458*1e2 #cm/s
+m = 9.1093837*1e-31*1e3 #g
+e = 4.80320425*1e-10  # statCoulomb
+
+### Smilei parameters
+# references
+wl = 0.8e-4 # cm
+omega_r = 2*np.pi*c/wl # rad/s
+
+# conversions
+Nr = m*omega_r**2/(np.pi*4*e**2) # 1/cm³
+Kr = 0.51099895000 # m*c**2, MeV 
+Lr = c/omega_r # cm/2*pi
+Tr = 1/omega_r # s/2*pi
+Er = m*c*omega_r/e #statV/cm = (sqrt(g/cm)/s)
+
+dx = 2*np.pi/64 # longitudinal resolution
+Lx = 16*32*32*dx #256*dx
+dt = dx/4         # timestep
+
+
+figw = style.figsize['inch']['double_column_width']*0.6
+fig, ax = plt.subplots(2,2,figsize=(figw,figw*0.8))
+cmap = matplotlib.cm.get_cmap('coolwarm')
+colors_pp = [cmap(i) for i in np.linspace(0.,1,4)] #['tab:blue','tab:orange','tab:green','tab:red']
+
+fields = ['Ex','rho']
+axes = ['x_axis']#,'t_axis']
+x_sr =[0.0191e4,0.0193e4] #[0.0051e4,0.0056e4]#   
+
+i = 450 + 100 
+print('pipic',100*dt*Tr*i)
+f,a = load_data('./0,5e10_dx0,25_corr_delayed/lwfa_neg.h5',fields=fields,axes=axes,ind=i+1)
+Ex,rho = f
+x_axis = a[0]
+x_axis *= 1e4
+x_axis += 1e4*Lx*Lr/2
+
+ax[0,0].plot(x_axis,Ex[i]*1e-6,color='k',label=r'$\pi$PIC - EC pusher')
+ax[1,0].plot(x_axis,rho[i]*1e-18,color='k')
+
+ix1 = np.argwhere(x_sr[0]<x_axis)[0][0]
+ix2 = np.argwhere(x_sr[1]<x_axis)[0][0]
+
+
+ax[0,1].plot(x_axis[ix1:ix2],Ex[i][ix1:ix2]*1e-6,color='k')
+ax[1,1].plot(x_axis[ix1:ix2],rho[i][ix1:ix2]*1e-18,color='k')
+
+'''
+f,a = load_data('./boris/lwfa_neg_delayed.h5',fields=fields,axes=axes,ind=i+1)
+Ex,rho = f
+x_axis = a[0]
+x_axis *= 1e4
+x_axis += 1e4*Lx*Lr/2
+
+ax[0,0].plot(x_axis,Ex[i]*1e-6,'--',color='tab:orange',label=r'$\pi$PIC - Boris pusher')
+ax[1,0].plot(x_axis,rho[i]*1e-18,'--',color='tab:orange')
+
+ax[0,1].plot(x_axis[ix1:ix2],Ex[i][ix1:ix2]*1e-6,'--',color='tab:orange')
+ax[1,1].plot(x_axis[ix1:ix2],rho[i][ix1:ix2]*1e-18,'--',color='tab:orange')
+'''
+
+S = happi.Open('../../../../smilei/benchmark/1d/4res_M4_solver/')
+
+fwhm_duration_laser_pulse = 15*2*np.pi
+pulse_duration = fwhm_duration_laser_pulse*2*3/2.335 #+- 3sigma 
+Lx = 16*32*32*dx #256*dx
+distance_laser_peak_window_border = 1.7*fwhm_duration_laser_pulse
+propagation_time = pulse_duration/2 + distance_laser_peak_window_border 
+
+#s = 2783 
+
+s = int((propagation_time/dt + i*100)/20) 
+
+print('Propagation time',propagation_time*Tr)
+print('smilei',dt*Tr*s*20 - propagation_time*Tr)
+print('pipic',100*dt*Tr*i)
+print('diff', 100*dt*i - (dt*s*20 - propagation_time))
+print(dt*8*2)
+
+
+Rho = S.Field.Field0("Rho",timestep_indices=s).getData()
+Ey = S.Field.Field0("Ey",timestep_indices=s).getData()
+Ex = S.Field.Field0("Ex",timestep_indices=s).getData()
+Ez = S.Field.Field0("Ez",timestep_indices=s).getData()
+
+x = np.arange(0,Lx*Lr+dx*Lr,dx*Lr) 
+x -= 0 # - c*8*dt*Tr # compensation for diffrent starting points?
+x *= 1e4
+ax[0,0].plot(x,Er*Ex[0]*1e-6,'-.',color='tab:green',label='Smilei - Boris pusher')
+ax[1,0].plot(x,-Nr*Rho[0]*1e-18,'-.',color='tab:green')
+
+ix1 = np.argwhere(x_sr[0]<x)[0][0]
+ix2 = np.argwhere(x_sr[1]<x)[0][0]
+
+ax[0,1].plot(x[ix1:ix2],Er*Ex[0][ix1:ix2]*1e-6,'-.',color='tab:green')
+ax[1,1].plot(x[ix1:ix2],-Nr*Rho[0][ix1:ix2]*1e-18,'-.',color='tab:green')
+#ax[1,1].vlines(Lx*Lr*1e4/4,0,0.01)
+
+ylim = ax[0,1].get_ylim()
+ax[0,0].fill_between([x_sr[0],x_sr[1]],[ylim[0],ylim[0]],[ylim[1],ylim[1]],
+                      facecolor=(1,0,0,0), 
+                      edgecolor=(0,0,0,1),
+                      zorder=100,
+                      lw=1)
+
+
+ylim = ax[1,1].get_ylim()
+ax[1,0].fill_between([x_sr[0],x_sr[1]],[ylim[0],ylim[0]],[ylim[1],ylim[1]],
+                      facecolor=(1,0,0,0), 
+                      edgecolor=(0,0,0,1),
+                      zorder=100,
+                      lw=1)
+
+ax[0,0].set_ylim(-5,5)
+ax[1,0].set_ylim(0,3)
+
+ax[0,0].tick_params(which='both',direction='in',
+                    labelbottom=False)
+ax[0,0].set_ylabel(r'$E_x$ [$10^{6}$statV/cm]')
+
+
+ax[1,0].tick_params(which='both',direction='in')
+ax[1,0].set_ylabel(r'$\rho$ [$10^{18}$cm$\,^{-3}$]')
+ax[1,0].set_xlabel(r'$z$ ($\mu$m)')
+
+ax[0,1].set_ylabel(r'$E_x$ [$10^{6}$statV/cm]')
+ax[0,1].yaxis.tick_right()
+ax[0,1].yaxis.set_label_position("right")
+ax[0,1].tick_params(which='both',direction='in',
+                    labelbottom=False)
+
+ax[1,1].set_ylabel(r'$\rho$ [$10^{18}$cm$\,^{-3}$]')
+ax[1,1].yaxis.tick_right()
+ax[1,1].yaxis.set_label_position("right")
+ax[1,1].tick_params(which='both',direction='in')
+ax[1,1].set_xlabel(r'$z$ ($\mu$m)')
+
+#ax[0,0].set_yticks([-1,1,3])
+#ax[1,0].set_yticks([0.4,1.5,2,2.5])
+#ax[1,1].set_yticks([0.3,0.7,1.1,1.5])
+
+ax[0,0].set_xlim(80,205)
+ax[1,0].set_xlim(80,205)
+ax[0,1].set_xlim(x_sr[0],x_sr[1])
+ax[1,1].set_xlim(x_sr[0],x_sr[1])
+
+for i in range(2):ax[i,0].set_xticks([100,140,180])
+for i in range(2):ax[i,1].set_xticks([191.3,192.1,192.8])
+ax[1,0].set_yticks([0,1,2])
+
+t = ['(a)','(b)','(c)','(d)']
+ax[0,0].text(0.05,0.87,t[0],transform=ax[0,0].transAxes)
+ax[1,0].text(0.05,0.87,t[1],transform=ax[1,0].transAxes)
+ax[0,1].text(0.83,0.87,t[2],transform=ax[0,1].transAxes)
+ax[1,1].text(0.05,0.87,t[3],transform=ax[1,1].transAxes)
+
+
+fig.legend(frameon=False,ncol=3, bbox_to_anchor=(0.95, 1.03))
+
+plt.subplots_adjust(wspace=0.05, hspace=0.05)
+
+plt.tight_layout()
+plt.savefig('im.png',dpi=600)
+
+
+
